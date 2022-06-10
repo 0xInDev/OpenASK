6fa9238471ca424a68c1d99cbc35f01751e5d069
ssakone <abdramanesakone1@gmail.com>
2022-06-10 20:02:31 +0000

    update base dir

--- api/models.py
+++ api/models.py
@@ -7,57 +7,46 @@ def logged_user(request):
 
 
 class Sondage(models.Model):
-    state = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
     sondage = models.CharField(max_length=255)
-    description = models.CharField(max_length=255, null=True)
-    theme = models.JSONField(default=dict)
+    description = models.Text(max_length=255, null=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.RESTRICT, default=None)
-
+    
+    strictValidation = models.BooleanField(default=False)
+    state = models.IntegerField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.sondage
 
 
 class Question(models.Model):
-    state = models.BooleanField(default=True)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    
     sondage = models.ForeignKey('Sondage', on_delete=models.RESTRICT)
     question = models.CharField(max_length=255)
     description = models.CharField(max_length=255, null=True)
     type = models.IntegerField(
         choices=((0, 'CHOICE'), (1, 'CHOICES'), (3, 'NUMBER'), (2, 'TEXT')), default=0)
 
+    state = models.IntegerField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.question
 
-
-class QuestionLabel(models.Model):
-    state = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    question = models.ForeignKey('Question', on_delete=models.RESTRICT)
-    label = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.label
-
-
 class Answer(models.Model):
     state = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    questionLabel = models.ForeignKey(
-        'QuestionLabel', on_delete=models.RESTRICT, blank=True, null=True)
+    
     question = models.ForeignKey(
         'Question', on_delete=models.RESTRICT, blank=True, null=True)
     response = models.CharField(max_length=255, null=True, default="")
     email = models.CharField(max_length=255)
 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
-        return self.questionLabel
+        return self.question
 
 
--- django_backend/settings.py
+++ django_backend/settings.py
@@ -10,12 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
 import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
