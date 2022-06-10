eae132663d3a1d2e752d00b6a24fcbf48eddf0dd
ssakone <abdramanesakone1@gmail.com>
2022-06-10 20:05:40 +0000

    Fix sqlite database path

--- django_backend/settings.py
+++ django_backend/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
 
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
